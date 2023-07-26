5480ff1a18cb8fe77a3751dfad91ce1a8a9adc77
ramses.ramirez <ramses.ramirez@exodus.com.mx>
2023-07-26 17:48:20 -0600

    ProductCard & ProductSlide implemented

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import { Shop } from '@mui/icons-material'
 import type { NextPage } from 'next'
 import ShopLayout from '../components/layouts/ShopLayout'
-import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import { initialData } from '../database/products'
+import { ProductList } from '../components/products'
+import { IProduct } from '../interfaces'
 
 const Home: NextPage = () => {
   return (
@@ -10,22 +11,8 @@ const Home: NextPage = () => {
       <Typography variant='h1' component={'h1'}>Home</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
 
-      <Grid container spacing={4}>
-        {
-          initialData.products.map((product) => (
-            <Grid item xs={6} sm={4} key={product.slug}>
-              <Card>
-                <CardActionArea>
-                  <CardMedia 
-                    component={ 'img' }
-                    image={ `products/${product.images[0]}` }
-                    alt={ product.title }
-                  />
-                </CardActionArea>
-              </Card>
-            </Grid>
-        ))}
-      </Grid>
+      <ProductList products={ initialData.products as any } />
+
     </ShopLayout>
   )
 }
