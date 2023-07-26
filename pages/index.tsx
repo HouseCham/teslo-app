f6ab95515dcd0980f3cb3c8ce4f58327e56f2e9a
ramses.ramirez <ramses.ramirez@exodus.com.mx>
2023-07-26 08:55:48 -0600

    first home page implementation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,31 @@
 import { Shop } from '@mui/icons-material'
 import type { NextPage } from 'next'
 import ShopLayout from '../components/layouts/ShopLayout'
-import { Typography } from '@mui/material'
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
+import { initialData } from '../database/products'
 
 const Home: NextPage = () => {
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Los mejores productos :)'}>
       <Typography variant='h1' component={'h1'}>Home</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map((product) => (
+            <Grid item xs={6} sm={4} key={product.slug}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia 
+                    component={ 'img' }
+                    image={ `products/${product.images[0]}` }
+                    alt={ product.title }
+                  />
+                </CardActionArea>
+              </Card>
+            </Grid>
+        ))}
+      </Grid>
     </ShopLayout>
   )
 }
